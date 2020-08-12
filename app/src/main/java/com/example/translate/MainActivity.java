baf55f3ee090bf9c3215582aacd1865f350d7e48
direyesm <37608963+direyesm@users.noreply.github.com>
2020-08-11 22:06:18 -0400

    Nueva Rama

--- app/src/main/java/com/example/translate/MainActivity.java
+++ app/src/main/java/com/example/translate/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.translate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.prathameshmore.toastylibrary.Toasty;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toasty toasty = new Toasty(MainActivity.this);
+        Button boton = findViewById(R.id.btnToasty);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toasty.darkToasty(v.getContext(), "Apretaste el boton :D", Toasty.LENGTH_LONG, Toasty.BOTTOM);
+            }
+
+        });
+
+        }
+
+
+    protected void onStart () {
+        super.onStart();
+        Timber.d("onStart");
     }
 }
+
+
